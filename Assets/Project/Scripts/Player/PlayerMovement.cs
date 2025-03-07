c7df6dfe27ddc65d792ce0ac17377c0718eebf04
Felipe Maia <felipeemaia4@gmail.com>
2025-03-07 15:13:35 -0300

    Player character movement

--- Assets/Project/Scripts/Player/PlayerMovement.cs
+++ Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -1,22 +1,72 @@
 using UnityEngine;
+using DG.Tweening;
+using System;
 
 namespace TypemUp.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField] int[] rowsPositions;
-        [SerializeField] int activeRow = 2;
+        [Header("Rows")]
+        [SerializeField] float[] _rowsPositions;
+        [SerializeField] int _activeRow = 2;
 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        [Header("Animation")]
+        [SerializeField] float _duration;
+        [SerializeField] float _jumpPower;
+
+        public Action<int> OnRowChange;
+
+        bool canMove = true;
+
+
+        private void TryChangeRows(int direction)
         {
+            if (!canMove) return;
 
+            int lastRow = _rowsPositions.Length - 1;
+            int imaginaryRow = _activeRow + direction;
+            int newRow = Mathf.Clamp(imaginaryRow, 0, lastRow);
+
+            if (newRow != _activeRow)
+            {
+                float yPos = transform.position.y;
+                float xPos = _rowsPositions[newRow];
+
+                Vector2 newPosition = new Vector2(xPos, yPos);
+
+                canMove = false;
+                transform.DOJump(newPosition, _jumpPower, 1, _duration)
+                    .OnComplete(() => ChangeRow(newRow));
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void ChangeRow(int newRow)
         {
+            _activeRow = newRow;
+            canMove = true;
 
+            OnRowChange?.Invoke(_activeRow);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                TryChangeRows(-1);
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+                TryChangeRows(1);
+        }
+
+        private void Start()
+        {
+            canMove = true;
+
+            _activeRow = 2;
+
+            float yPos = transform.position.y;
+            float xPos = _rowsPositions[_activeRow];
+
+            Vector2 newPosition = new Vector2(xPos, yPos);
+            transform.position = newPosition;
         }
     }
-}
+} 
